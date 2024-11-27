52228ec237d74976c87fb19a2f0f550c4a4ffc39
SiarheiLabodzin <siarhei.labodzin@innowise.com>
2024-11-27 12:01:41 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'purpleButton': '#793CFB',
+      },
+    },
+    fontFamily: {
+      poppins: "'Poppins', sans-serif",
+    },
+    
+  },
+  plugins: [],
+}
+
